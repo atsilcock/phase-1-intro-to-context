89a928de491d6686bb1dd8c06ff69ee8b7d87bbb
Alin Silcock <alinsilcock@Alin-MacBook-Air.local>
2024-01-04 07:35:21 -0700

    intro to context

--- index.js
+++ index.js
@@ -1,1 +1,78 @@
-// Your code here
+
+
+// Function 1
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    return {
+      firstName,
+      familyName,
+      title,
+      payPerHour,
+      timeInEvents: [],
+      timeOutEvents: [],
+    };
+  }
+  
+  // Function 2
+  function createEmployeeRecords(employeeData) {
+    return employeeData.map(createEmployeeRecord);
+  }
+  
+  // Function 3
+  function createTimeInEvent(employee, dateStamp) {
+    const [date, hour] = dateStamp.split(' ');
+    employee.timeInEvents.push({
+      type: 'TimeIn',
+      hour: parseInt(hour, 10),
+      date,
+    });
+    return employee;
+  }
+  
+  // Function 4
+  function createTimeOutEvent(employee, dateStamp) {
+    const [date, hour] = dateStamp.split(' ');
+    employee.timeOutEvents.push({
+      type: 'TimeOut',
+      hour: parseInt(hour, 10),
+      date,
+    });
+    return employee;
+  }
+  
+  // Function 5
+  function hoursWorkedOnDate(employee, date) {
+    const timeIn = employee.timeInEvents.find((event) => event.date === date);
+    const timeOut = employee.timeOutEvents.find((event) => event.date === date);
+    return (timeOut.hour - timeIn.hour) / 100;
+  }
+  
+  // Function 6
+  function wagesEarnedOnDate(employee, date) {
+    const hoursWorked = hoursWorkedOnDate(employee, date);
+    return hoursWorked * employee.payPerHour;
+  }
+  
+  // Function 7
+  function allWagesFor(employee) {
+    const allDates = employee.timeInEvents.map((event) => event.date);
+    const totalWages = allDates.reduce((total, date) => total + wagesEarnedOnDate(employee, date), 0);
+    return totalWages;
+  }
+  
+  // Function 8
+  function calculatePayroll(employees) {
+    return employees.reduce((total, employee) => total + allWagesFor(employee), 0);
+  }
+  
+  // Export the functions
+  module.exports = {
+    createEmployeeRecord,
+    createEmployeeRecords,
+    createTimeInEvent,
+    createTimeOutEvent,
+    hoursWorkedOnDate,
+    wagesEarnedOnDate,
+    allWagesFor,
+    calculatePayroll,
+  };
+  
